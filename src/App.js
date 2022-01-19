aaff89748c591dda72434de519fde99ee93767e2
Markeisha <markeisha94@gmail.com>
2022-01-19 13:43:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+import Dictionary from './components/Dictionary';
+
 function App() {
+  const [data, setData] = useState('')
+  const [searchWord, setSearchWord] = useState('')
+
+  const getMeaning = () => {
+    axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/<word>')
+      .then(resp => {
+        setData(resp.data[0])
+      })
+  }
+
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Dictionary App</h1>
+      <div className='search-box'>
+        <input 
+          type='text'
+          placeholder='Search'
+          onChange={(e) => {
+            setSearchWord(e.target.value)
+          }}
+        />
+        <button
+          onClick={() => {
+            getMeaning()
+          }}
+        />
+      </div>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
 import App from './App';
 import './index.css';
 
+import { reducer } from './reducers';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
